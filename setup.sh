2f5511bacee50c5baa8602910d88a04de1bb04f6
msaivenkatasiva <sivasl0597@gmail.com>
2026-03-05 11:11:43 +0530

    setup

---
+++ setup.sh
@@ -0,0 +1,93 @@
+# #!bin/bash
+
+# aws eks update-kubeconfig --name expense-dev --region us-east-1
+
+
+# # IAM Permissions
+# eksctl utils associate-iam-oidc-provider \
+#     --region us-east-1 \
+#     --cluster expense-dev \
+#     --approve
+
+# curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.1.2/docs/install/iam_policy.json
+
+# aws iam create-policy \
+#     --policy-name AWSLoadBalancerControllerIAMPolicy \
+#     --policy-document file://iam-policy.json
+
+# eksctl create iamserviceaccount \
+# --cluster=expense-dev \
+# --namespace=kube-system \
+# --name=aws-load-balancer-controller \
+# --attach-policy-arn=arn:aws:iam::210749645231:policy/AWSLoadBalancerControllerIAMPolicy \
+# --override-existing-serviceaccounts \
+# --approve
+
+# curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.1.2/docs/install/iam_policy.json
+
+# # Add Controller to Cluster
+# helm repo add eks https://aws.github.io/eks-charts
+
+# kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
+
+# get_vpc_id
+# aws eks describe-cluster \
+#   --name expense-dev \
+#   --query "cluster.resourcesVpcConfig.vpcId" \
+#   --output text
+
+# helm install aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=expense-dev --set region=us-east-1 --set region=us-east-1 --set vpcId=
+
+
+#!/bin/bash
+
+CLUSTER_NAME=expense-dev
+REGION=us-east-1
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+
+echo "Associating OIDC Provider..."
+
+eksctl utils associate-iam-oidc-provider \
+--cluster $CLUSTER_NAME \
+--approve
+
+echo "Downloading IAM policy..."
+
+curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json
+
+echo "Creating IAM policy..."
+
+aws iam create-policy \
+--policy-name AWSLoadBalancerControllerIAMPolicy \
+--policy-document file://iam_policy.json || true
+
+echo "Creating IAM ServiceAccount..."
+
+eksctl create iamserviceaccount \
+--cluster $CLUSTER_NAME \
+--namespace kube-system \
+--name aws-load-balancer-controller \
+--attach-policy-arn arn:aws:iam::$ACCOUNT_ID:policy/AWSLoadBalancerControllerIAMPolicy \
+--approve \
+--region $REGION
+
+echo "Installing AWS Load Balancer Controller..."
+
+helm repo add eks https://aws.github.io/eks-charts
+helm repo update
+
+VPC_ID=$(aws eks describe-cluster --name $CLUSTER_NAME --query "cluster.resourcesVpcConfig.vpcId" --output text)
+
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
+-n kube-system \
+--set clusterName=$CLUSTER_NAME \
+--set region=$REGION \
+--set serviceAccount.create=false \
+--set serviceAccount.name=aws-load-balancer-controller \
+--set vpcId=$VPC_ID
+
+echo "Creating namespace..."
+
+kubectl create namespace expense || true
+
+echo "Setup Complete!"
